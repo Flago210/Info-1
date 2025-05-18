2e93aea014411439df676f7c5012b00933033af2
Flago210 <retamozos291@gmail.com>
2025-05-18 19:37:56 -0300

    subida del archivo de 4to trabajo

---
+++ Punto_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	float NOTA;
+	//Nombre: Santiago Retamozo
+	//Legajo: 98417
+	//pedir la nota al usuario
+	printf ("Ingrese su calificacion (0 a 100): ");
+	scanf ("%f", &NOTA);
+	//muestra de calificacion
+	if (NOTA >=90) {
+		printf ("Calificacion: A");}
+	else if (NOTA >=80) {
+		printf ("Calificacion: B");}
+	else if (NOTA >= 70) {
+		printf ("Calificacion: C");}
+	else if (NOTA >= 60) {
+		printf ("Calificacion: D");}
+	else{ 
+		printf ("Calificacion: F");}
+	return 0;
+}
+
